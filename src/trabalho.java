15e467be6394bb076a84f4a1512c3ee3354a3c1c
eTrinityE <contaduduzi@gmail.com>
2020-05-08 14:27:52 -0300

    Relatorio gerado

--- src/trabalho.java
+++ src/trabalho.java
@@ -8,6 +8,7 @@ public class trabalho {
 		
 		char sexo;
 		int cerveja, refri, espeto;
+		double consumo, couvert, ingresso, total;
 		
 		System.out.println("Sexo: ");
 		sexo = sc.next().charAt(0);
@@ -17,6 +18,34 @@ public class trabalho {
 		refri = sc.nextInt();
 		System.out.println("Quantidade de espetinhos: ");
 		espeto = sc.nextInt();
+		
+		if (sexo == 'F') {
+			ingresso = 8.0;
+		}else {
+			ingresso = 10.0;
+		}
+		
+		consumo = cerveja * 5.0 + refri * 3.0 + espeto * 7.0;
+		
+		if (consumo > 30) {
+			couvert = 0.00;
+		}else {
+			couvert = 4.0;
+		}
+		
+		total = ingresso + consumo + couvert;
+		
+		System.out.println();
+		System.out.println("RELATÓRIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if (couvert == 0) {
+			System.out.println("Isento de Couvert");
+		}else {
+			System.out.printf("Couvert = R$ %.2f%n", couvert);
+		}
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n", total);
 		
 		sc.close();
 	}
